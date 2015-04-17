8b7e3d5a2ef2c84455a54c251255c3cfbe1d3e7c
patrick <student@epicodus.com>
2015-04-17 11:27:24 -0700

    write html

--- spec/specs.js
+++ spec/specs.js
@@ -1,39 +1,33 @@
 describe('pizzaOrder', function() {
 
-    describe("topping", function() {
-        it("will return 'Cheese Pizza' if 'Cheese' is selected", function() {
-            var testOrder = Object.create(pizzaOrder);
-            testOrder.pepperoni_pizza = false;
-            testOrder.topping("Cheese");
-            expect(testOrder.topping).to.equal("Cheese");
-        });
-
-        it("will return 'Pepperoni Pizza' if 'Pepperoni' is selected", function() {
-            var testOrder = Object.create(pizzaOrder);
-            testOrder.pepperoni_pizza = true;
-            testOrder.topping("Pepperoni");
-            expect(testOrder.topping).to.equal("Pepperoni");
-        });
+  describe("topping", function() {
+    it("will return 'Cheese Pizza' if 'Cheese' is selected", function() {
+      var testOrder = Object.create(pizzaOrder);
+      testOrder.pepperoni_pizza = false;
+      testOrder.topping("Cheese");
+      expect(testOrder.topping).to.equal("Cheese");
     });
 
-    describe("price", function() {
-        it("will return '12.95' if 'Cheese' is selected", function() {
-            var testOrder = Object.create(pizzaOrder);
-            testOrder.pepperoni_pizza = false;
-            testOrder.topping("Cheese");
-            expect(testOrder.balance).to.equal(12.95)
-        });
-
-        it("will return '14.95' if 'Pepperoni' is selected", function() {
-            var testOrder = Object.create(pizzaOrder);
-            testOrder.pepperoni_pizza = true;
-            testOrder.topping("Pepperoni");
-            expect(testOrder.balance).to.equal(14.95)
-        });
+    it("will return 'Pepperoni Pizza' if 'Pepperoni' is selected", function() {
+      var testOrder = Object.create(pizzaOrder);
+      testOrder.pepperoni_pizza = true;
+      testOrder.topping("Pepperoni");
+      expect(testOrder.topping).to.equal("Pepperoni");
+    });
+  });
+  describe("price", function() {
+    it("will return '12.95' if 'Cheese' is selected", function() {
+      var testOrder = Object.create(pizzaOrder);
+      testOrder.pepperoni_pizza = false;
+      testOrder.topping("Cheese");
+      expect(testOrder.balance).to.equal(12.95)
     });
 
-
-
-
-
+    it("will return '14.95' if 'Pepperoni' is selected", function() {
+      var testOrder = Object.create(pizzaOrder);
+      testOrder.pepperoni_pizza = true;
+      testOrder.topping("Pepperoni");
+      expect(testOrder.balance).to.equal(14.95)
+    });
+  });
 });
